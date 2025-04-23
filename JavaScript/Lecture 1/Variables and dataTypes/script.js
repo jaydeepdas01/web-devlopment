941141cc0c52f6262f5057eb622cf0811ec08d10
jaydeepdas01 <97689637+jaydeepdas01@users.noreply.github.com>
2025-04-23 09:53:34 +0530

    netflix clone for practics
    netflix cline

---
+++ JavaScript/Lecture 1/Variables and dataTypes/script.js
@@ -0,0 +1,36 @@
+let name  = "My name is Jaydeep Das";
+console.log(name);
+
+let age= 21;
+console.log(age);
+
+let price = 25.55;
+console.log(price); 
+
+//check typeof
+let a = 1; //go to console and check typeof a
+
+let b = "name"; //string
+
+let c  = true; // boolean/
+
+let d = null; //object 
+
+let e = BigInt("123"); //big int
+
+let f = Symbol("Hello"); //symbol 
+
+
+//object
+ const student = {
+    fullName: "jaydeep das ",
+    age: 21,
+    cgpa: 8,
+    isPass: true,
+ } ;
+//object value print
+ console.log(student.fullName);
+ console.log(student.age);
+ console.log(student.cgpa);
+ console.log(student.isPass);
+
---
+++ JavaScript/Lecture 1/first code/first.js
@@ -0,0 +1,2 @@
+console.log("Hello my name is jaydeep das");
+console.log("welcome");
